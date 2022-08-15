547e11d7a2de257674b48bf7eed6d2cba17fdc94
ADlv19 <adwaitg02@gmail.com>
2022-08-15 15:58:57 +0530

    Rest Row

---
+++ components/RestaurantRow.jsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+const RestaurantRow = () => {
+    return (
+        <View>
+            <Text>RestaurantRow</Text>
+        </View>
+    );
+};
+
+export default RestaurantRow;
